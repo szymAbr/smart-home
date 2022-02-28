23dc13e3c0e5aa6b7a6386e324c38d1c86cec0ac
szymAbr <sz.abramowski@gmail.com>
2022-02-28 22:09:36 +0100

    fix footer position

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="d-flex flex-column align-items-center fixed-bottom">
+    <footer className="d-flex flex-column align-items-center mt-5">
       <p>Author: Szymon Abramowski</p>
       <p>
         <a href="https://github.com/szymAbr" target="_blank" rel="noreferrer">
